927f5299da97a34764636ff72fe0668153e01b48
sagarkrsd <sagar.kumar@openebs.io>
2018-07-03 00:43:54 +0530

    Add codecov report for CITF
    Changes committed:
    	new file:   .gitignore
    	modified:   .travis.yml
    	modified:   Makefile
    	modified:   README.md
    	new file:   test.sh
    
    Signed-off-by: sagarkrsd <sagar.kumar@openebs.io>

---
+++ test.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+#
+# This script runs tests and generates a report file.
+
+set -e
+echo "" > coverage.txt
+PACKAGES=$(go list ./... | grep -v '/vendor/')
+for d in $PACKAGES; do
+	go test -coverprofile=profile.out -covermode=atomic $d
+	if [ -f profile.out ]; then
+		cat profile.out >> coverage.txt
+		rm profile.out
+	fi
+done
